084cdd91d18a6a600aa7d7cafdf76588f7d797ca
Ajay Mondal <mrakmondal6612@gmail.com>
2024-09-21 23:34:52 +0530

    icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className="px-6 md:px-0 mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">Mr. AK</h1>
       <div className="flex space-x-6 mt-4">
-        <a className="hover:text-gray-300" href="#">
+        <a className="hover:text-gray-300" href="https://www.instagram.com/b.tech_ak/">
           <FaInstagram size={24} />
         </a>
-        <a className="hover:text-gray-300" href="#">
+        <a className="hover:text-gray-300" href="https://www.linkedin.com/in/mr-ak/">
           <FaLinkedin size={24} />
         </a>
         <a className="hover:text-gray-300" href="#">
