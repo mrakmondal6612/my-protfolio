5fdaa93e1ed18b0491e8dcc54a550d387c7c7ac5
Ajay Mondal <mrakmondal6612@gmail.com>
2024-09-21 22:41:55 +0530

    changing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { title } from "process";
-import path from "path";
+
 
 const navLinks = [
   { title: "About", path: "#about" },
